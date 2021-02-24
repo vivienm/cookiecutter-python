95611a3cac00b23edd55f8a6d60085562866fc69
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2021-02-24 11:40:11 +0100

    Add noxfile.py

---
+++ {{cookiecutter.name}}/noxfile.py
@@ -0,0 +1,34 @@
+import nox  # type: ignore
+
+
+nox.options.default_venv_backend = "none"
+
+
+@nox.session
+def install(session):
+    session.run_always("poetry", "install")
+
+
+@nox.session
+def black(session):
+    session.run("poetry", "run", "black", "--check", ".")
+
+
+@nox.session
+def isort(session):
+    session.run("poetry", "run", "isort", "--check", ".")
+
+
+@nox.session
+def mypy(session):
+    session.run("poetry", "run", "mypy", ".")
+
+
+@nox.session
+def flake8(session):
+    session.run("poetry", "run", "flake8")
+
+
+@nox.session
+def pytest(session):
+    session.run("poetry", "run", "pytest")

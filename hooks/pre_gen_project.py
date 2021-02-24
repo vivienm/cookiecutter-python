10bcd730fa7049429f104c7c5679e7930350fbb4
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2021-02-24 13:28:05 +0100

    Hide *_name variables

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,7 @@
+"""
+{{ cookiecutter.update({
+    "dir_name": cookiecutter.name.lower(),
+    "bin_name": cookiecutter.name.lower(),
+    "lib_name": cookiecutter.name.lower().replace("-", "_"),
+}) }}
+"""

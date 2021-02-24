ac5a56d87473a02ed6654e9af408202f8ac45d4d
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2021-02-24 11:09:41 +0100

    Improve hook script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,36 +1,64 @@
 import shutil
 import subprocess
 from pathlib import Path
+from typing import Collection
 
-DEV_DEPS = [
+DEV_DEPS = (
     "black",
     "flake8",
     "flake8-commas",
     "flake8-docstrings",
     "isort",
     "mypy",
-    "nox",
     "pytest",
-]
-subprocess.run(
-    ["poetry", "add", "-n", "--dev"] + DEV_DEPS,
-    check=True,
 )
 
 
-if "{{ cookiecutter.template }}" == "bin":
-    subprocess.run(
-        ["poetry", "add", "-n", "click"],
+LIB_DEPS = ()
+
+
+BIN_DEPS = ("click",)
+
+
+def poetry_add(args: Collection[str]) -> None:
+    if len(args) != 0:
+        subprocess.run(
+            ["poetry", "add", "-n", *args],
+            check=True,
+        )
+
+
+def install_dev_dependencies() -> None:
+    poetry_add(["--dev", *DEV_DEPS])
+
+
+def install_lib_dependencies() -> None:
+    poetry_add(LIB_DEPS)
+
+
+def install_bin_dependencies() -> None:
+    poetry_add(BIN_DEPS)
+
+
+def remove_poetry_venv() -> None:
+    venv_path = subprocess.run(
+        ["poetry", "env", "info", "-p"],
+        stdout=subprocess.PIPE,
         check=True,
-    )
-else:
-    Path("src/{{ cookiecutter.lib_name }}/__main__.py").unlink()
+        text=True,
+    ).stdout.rstrip()
+    shutil.rmtree(venv_path)
 
 
-venv_path = subprocess.run(
-    ["poetry", "env", "info", "-p"],
-    stdout=subprocess.PIPE,
-    check=True,
-    text=True,
-).stdout.rstrip()
-shutil.rmtree(venv_path)
+def main() -> None:
+    install_lib_dependencies()
+    if "{{cookiecutter.template}}" == "bin":
+        install_bin_dependencies()
+    else:
+        Path("src/{{cookiecutter.lib_name}}/__main__.py").unlink()
+    install_dev_dependencies()
+    remove_poetry_venv()
+
+
+if __name__ == "__main__":
+    main()
